c2f21090d8033db8c3875085de0b3ab104a76ac6
Martin Boros <mboros04@gmail.com>
2022-04-19 11:08:44 -0400

    updates project files, places '@' on screen with movement keys

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 
 #include <SDL.h>
-#include <libtcod.h>
+#include <libtcod.hpp>
 
 #include <cstdlib>
 
@@ -12,9 +12,9 @@ int main(int argc, char** argv) {
   params.renderer_type = TCOD_RENDERER_SDL2;
   params.vsync = 1;
   params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
-  params.window_title = "Libtcod Template Project";
+  params.window_title = "Libtcod CPP Tutorial";
 
-  auto tileset = tcod::load_tilesheet("data/dejavu16x16_gs_tc.png", {32, 8}, tcod::CHARMAP_TCOD);
+  auto tileset = tcod::load_tilesheet("../data/dejavu16x16_gs_tc.png", {32, 8}, tcod::CHARMAP_TCOD);
   params.tileset = tileset.get();
 
   auto console = tcod::Console{80, 40};
@@ -22,11 +22,16 @@ int main(int argc, char** argv) {
 
   auto context = tcod::new_context(params);
 
+  int playerx=4, playery=4;
   // Game loop.
   while (true) {
     // Rendering.
     console.clear();
-    tcod::print(console, {0, 0}, "Hello World", TCOD_white, std::nullopt);
+    auto& player = console.at(playerx, playery);
+    player.ch = '@';
+    player.fg.b = 0x0;
+    player.fg.g = 0xFF;
+    player.fg.r = 0x0;
     context->present(console);
 
     // Handle input.
@@ -36,7 +41,25 @@ int main(int argc, char** argv) {
       switch (event.type) {
         case SDL_QUIT:
           std::exit(EXIT_SUCCESS);
-          break;
+        case SDL_KEYDOWN: {
+          auto keyPress = event.key.keysym.sym;
+          switch (keyPress) {
+            case SDLK_UP:
+              playery--;
+              break;
+            case SDLK_DOWN:
+              playery++;
+              break;
+            case SDLK_LEFT:
+              playerx--;
+              break;
+            case SDLK_RIGHT:
+              playerx++;
+              break;
+            default:
+              break;
+          }
+        }
       }
     }
   }
